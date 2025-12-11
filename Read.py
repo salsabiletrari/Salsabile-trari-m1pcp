f9e1986b24ff6243025151ec48a4ac0d46ccc76c
tekiasanae <tekiasanae1@gmail.com>
2025-12-12 00:15:10 +0100

    Update Read.py

--- Read.py
+++ Read.py
@@ -4,3 +4,13 @@
 #benmoussa siham
 #HiouliSarah
 #tekia sanae
+
+import pandas as pd
+
+
+# Données : Séquences ADN, Longueur, Pourcentage de GC
+data = {
+  "Séquence": ["ATGCGTACGTA", "GCTAGCTAGGCC", "ATGCGCGTAAGT", "TACGATCGTA", "ATGAAAGGCTT", "CGTACGTAGT", "TTAACCGGAT"],
+  "Longueur": [12, 12, 12, 10, 11, 10, 10], 
+  "Pourcentage GC": [50, 66.67, 58.33, 40, 45.45, 60, 50] 
+  } 
