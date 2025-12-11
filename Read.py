71679aa945165c0909eebc610e7eb2f12c9b0249
tekiasanae <tekiasanae1@gmail.com>
2025-12-12 00:26:12 +0100

    Update Read.py

--- Read.py
+++ Read.py
@@ -7,10 +7,9 @@
 
 import pandas as pd
 
-
 # Données : Séquences ADN, Longueur, Pourcentage de GC
 data = {
   "Séquence": ["ATGCGTACGTA", "GCTAGCTAGGCC", "ATGCGCGTAAGT", "TACGATCGTA", "ATGAAAGGCTT", "CGTACGTAGT", "TTAACCGGAT"],
   "Longueur": [12, 12, 12, 10, 11, 10, 10], 
-  "Pourcentage GC": [50, 66.67, 58.33, 40, 45.45, 60, 50] 
-  } 
+  "Pourcentage GC": [50, 66.67, 58.33, 40, 45.45, 60, 50]   } 
+} 
