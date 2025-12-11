37cf45d4fb034b8e6f8baa552bbcc3bbe75d37bb
salsabiletrari <salsabilritaj521@gmail.com>
2025-12-12 00:38:16 +0100

    Update Read.py

--- Read.py
+++ Read.py
@@ -13,3 +13,18 @@ data = {
   "Longueur": [12, 12, 12, 10, 11, 10, 10], 
   "Pourcentage GC": [50, 66.67, 58.33, 40, 45.45, 60, 50]   } 
 } 
+
+#1) Création d'un DataFrame (tableau pandas)
+df = pd.DataFrame(data)
+print("**************** Creation et affichage *****************")
+
+# Affichage du tableau
+print("Tableau des séquences ADN :", "\n" "\n")
+print(df, "\n" "\n")
+
+# Opérations sur les tableaux:
+print("************** Operations ***************")
+
+#2) Sélectionner la colonne "Longueur"
+Longueur = df["Longueur"]
+print(Longueur, "\n" "\n")
